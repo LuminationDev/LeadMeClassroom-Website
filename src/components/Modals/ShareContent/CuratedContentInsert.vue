961e0ecc5b592efc88f51d3fc9a5afbfa52a33f4
Edward Coad <ecoad@lumination.com.au>
2023-06-29 15:16:16 +0930

    Basic share content structure in place

--- src/components/Modals/ShareContent/CuratedContentInsert.vue
+++ src/components/Modals/ShareContent/CuratedContentInsert.vue
@@ -13,6 +13,10 @@ import ContentFilter from "@/components/CuratedContent/ContentFilter.vue";
 import CuratedContentListItem from "@/components/CuratedContent/CuratedContentListItem.vue";
 import {storeToRefs} from "pinia";
 
+defineEmits<{
+  (e: 'back'): void
+}>()
+
 const dashboardPinia = useDashboardStore();
 const showModal = ref(false);
 const shareType = ref("web");
@@ -288,199 +292,175 @@ const resetModal = () => {
 </script>
 
 <template>
-  <!--Anchor button used to control the modal-->
-  <button class="
-    w-56 h-9 flex justify-center items-center
-    bg-blue-500 hover:bg-blue-400
-    text-white text-base font-medium"
-    v-on:click="showModal = true"
-    id="share_button"
-  >
-    <img class="w-4 h-4 mr-3" src="/src/assets/img/session-icon-curated-content.svg" alt="Icon"/>
-    Curated content
-  </button>
+  <header class="h-20 px-8 w-modal-width-sm bg-white flex justify-between items-center rounded-t-lg">
+    <p class="text-2xl font-medium">Select content</p>
 
-  <!--Modal body using the Modal template, teleports the html to the bottom of the body tag-->
-  <Teleport to="body">
-    <Modal :show="showModal" @close="closeModal">
-      <template v-slot:header>
-        <header class="h-20 px-8 w-modal-width bg-white flex justify-between items-center rounded-t-lg">
-          <p class="text-2xl font-medium">Select content</p>
+    <div class="flex flex-row items-center">
+      <!--Allow a user to search via the input-->
+      <div class="flex items-center w-60 mr-4">
+        <label for="searchInput" class="cursor-pointer w-8 h-8 ml-1 absolute">
+          <img src="/src/assets/img/search-icon.svg" alt="Search"/>
+        </label>
+        <input
+            id="searchInput"
+            v-model="searchQuery"
+            placeholder="Search titles"
+            class="h-12 w-60 pr-6 pl-10 rounded-lg text-black bg-gray-100"/>
 
-          <div class="flex flex-row items-center">
-            <!--Allow a user to search via the input-->
-            <div class="flex items-center w-60 mr-4">
-              <label for="searchInput" class="cursor-pointer w-8 h-8 ml-1 absolute">
-                <img src="/src/assets/img/search-icon.svg" alt="Search"/>
-              </label>
-              <input
-                  id="searchInput"
-                  v-model="searchQuery"
-                  placeholder="Search titles"
-                  class="h-12 w-60 pr-6 pl-10 rounded-lg text-black bg-gray-100"/>
+        <img
+            v-if="searchQuery !== ''"
+            v-on:click="searchQuery = ''"
+            class="w-3 h-3 relative right-5 cursor-pointer"
+            src="/src/assets/img/cross.svg" alt="Search"/>
+      </div>
 
-              <img
-                  v-if="searchQuery !== ''"
-                  v-on:click="searchQuery = ''"
-                  class="w-3 h-3 relative right-5 cursor-pointer"
-                  src="/src/assets/img/cross.svg" alt="Search"/>
-            </div>
+      <!--Show the filter menu-->
+      <button v-on:click="showFilter = !showFilter">
+        <img src="/src/assets/icons/filter.svg">
+      </button>
 
-            <!--Show the filter menu-->
-            <button v-on:click="showFilter = !showFilter">
-              <img src="/src/assets/icons/filter.svg">
-            </button>
+      <img
+          v-on:click="closeModal"
+          class="w-4 h-4 ml-8 cursor-pointer"
+          src="/src/assets/img/modal-icon-exit.svg"
+          alt="Close Icon"
+      />
+    </div>
+  </header>
 
-            <img
-                v-on:click="closeModal"
-                class="w-4 h-4 ml-8 cursor-pointer"
-                src="/src/assets/img/modal-icon-exit.svg"
-                alt="Close Icon"
-            />
-          </div>
-        </header>
-      </template>
-
-      <template v-slot:content>
-        <div class="w-modal-width">
-
-          <div v-if="viewDescription === ''" class="flex flex-col h-96">
-            <!--Show the filter area-->
-            <div class="relative">
-              <div class="absolute right-0 h-80 z-10 my-8 ease-in-out duration-500"
-                   :class="{ 'translate-x-full': !showFilter, 'translate-x-0': showFilter }">
-                <ContentFilter
-                    :selected-Year="yearQuery"
-                    @yearQuery="updateYear"
-                    :selected-subject="subjectQuery"
-                    @subjectQuery="updateSubject"
-                    :subjects="subjectList"
-                    :selected-topic="topicQuery"
-                    @topicQuery="updateTopic"
-                    :topics="topicList"/>
-              </div>
-              <Transition name="fading">
-                <div v-if="showFilter" @click="showFilter = false" class="absolute h-96 w-modal-width bg-gray-300 z-5 bg-opacity-70"></div>
-              </Transition>
-            </div>
-
-            <!--Load in the curated content-->
-            <div class="max-h-96 shrink flex flex-col overflow-y-auto">
-              <!--List of curated content items-->
-              <CuratedContentListItem
-                  v-for="(content, index) in sortedCuratedContent"
-                  :key="index"
-                  :content-item="content"
-                  :selected="selectedItems.findIndex(item => item.getPackageName() === content.getLink()) !== -1"
-                  @select="addOrRemoveItem(content)">
-              </CuratedContentListItem>
-            </div>
-          </div>
-
-          <div class="mx-14 mt-8 h-20 bg-white flex items-center justify-between">
-            <p class="ml-8 text-lg font-medium mr-2">Share to</p>
-            <div class="flex justify-start">
-              <label class="mr-4 lg:mr-14 flex justify-between items-center">
-                <input class="h-5 w-5 mr-4" name="shareType" type="radio" v-model="shareType" value="web">
-                <span class="text-base">Web users</span>
-              </label>
-
-              <label class="flex justify-between items-center mr-8 lg:mr-20">
-                <input class="h-5 w-5 mr-4" name="shareType" type="radio" v-model="shareType" value="mobile">
-                <span class="text-base">Mobile users</span>
-              </label>
-            </div>
-
-            <div class="flex justify-start">
-              <label class="mr-4 lg:mr-14 flex justify-between items-center">
-                <input class="h-5 w-5 mr-4" name="shareTo" type="radio" v-model="shareTo" value="all">
-                <span class="text-base">All connected users</span>
-              </label>
-
-              <label class="flex justify-between items-center mr-8 lg:mr-20">
-                <input class="h-5 w-5 mr-4" name="shareTo" type="radio" v-model="shareTo" value="selected">
-                <span class="text-base">Select users</span>
-              </label>
-            </div>
-          </div>
+  <div class="w-modal-width-sm">
+    <div v-if="viewDescription === ''" class="flex flex-col h-96">
+      <!--Show the filter area-->
+      <div class="relative">
+        <div class="absolute right-0 h-80 z-10 my-8 ease-in-out duration-500"
+             :class="{ 'translate-x-full': !showFilter, 'translate-x-0': showFilter }">
+          <ContentFilter
+              :selected-Year="yearQuery"
+              @yearQuery="updateYear"
+              :selected-subject="subjectQuery"
+              @subjectQuery="updateSubject"
+              :subjects="subjectList"
+              :selected-topic="topicQuery"
+              @topicQuery="updateTopic"
+              :topics="topicList"/>
         </div>
-        <div class="w-modal-width max-h-64 overflow-y-auto">
+        <Transition name="fading">
+          <div v-if="showFilter" @click="showFilter = false" class="absolute h-96 w-modal-width bg-gray-300 z-5 bg-opacity-70"></div>
+        </Transition>
+      </div>
 
-          <!--Web followers-->
-          <div v-if="shareTo === 'selected' && shareType === 'web' && webFollowers.length"
-               class="mt-4 flex flex-row flex-wrap ml-10 mr-14">
-            <WebGridItem
-                v-for="follower in sortedWebFollowers"
-                :key="follower.getUniqueId()"
-                class="pl-4 pt-4 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6"
-                :webFollower="follower"
-                :controls="false"
-                @update="(value: boolean) => { handleFollowerSelection(follower.getUniqueId(), value) }"/>
-          </div>
+      <!--Load in the curated content-->
+      <div class="max-h-96 shrink flex flex-col overflow-y-auto">
+        <!--List of curated content items-->
+        <CuratedContentListItem
+            v-for="(content, index) in sortedCuratedContent"
+            :key="index"
+            :content-item="content"
+            :selected="selectedItems.findIndex(item => item.getPackageName() === content.getLink()) !== -1"
+            @select="addOrRemoveItem(content)">
+        </CuratedContentListItem>
+      </div>
+    </div>
 
-          <!--Mobile followers-->
-          <div v-else-if="shareTo === 'selected' && shareType === 'mobile' && mobileFollowers.length"
-               class="mt-4 flex flex-row flex-wrap ml-10 mr-14">
-            <MobileGridItem
-                v-for="follower in sortedMobileFollowers"
-                :key="follower.getUniqueId()"
-                class="pl-4 pt-4 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6"
-                :mobileFollower="follower"
-                :controls="false"
-                @update="(value: boolean) => { handleFollowerSelection(follower.getUniqueId(), value) }"/>
-          </div>
+    <div class="mx-14 mt-8 h-20 bg-white flex items-center justify-between">
+      <p class="ml-8 text-lg font-medium mr-2">Share to</p>
+      <div class="flex justify-start">
+        <label class="mr-4 lg:mr-14 flex justify-between items-center">
+          <input class="h-5 w-5 mr-4" name="shareType" type="radio" v-model="shareType" value="web">
+          <span class="text-base">Web users</span>
+        </label>
 
-          <!--No followers-->
-          <div class="flex justify-center items-center bg-gray-200 mx-14 mt-4 px-5 py-5" v-else-if="shareTo === 'selected'">
-            <span>No students connected</span>
-          </div>
+        <label class="flex justify-between items-center mr-8 lg:mr-20">
+          <input class="h-5 w-5 mr-4" name="shareType" type="radio" v-model="shareType" value="mobile">
+          <span class="text-base">Mobile users</span>
+        </label>
+      </div>
+
+      <div class="flex justify-start">
+        <label class="mr-4 lg:mr-14 flex justify-between items-center">
+          <input class="h-5 w-5 mr-4" name="shareTo" type="radio" v-model="shareTo" value="all">
+          <span class="text-base">All connected users</span>
+        </label>
+
+        <label class="flex justify-between items-center mr-8 lg:mr-20">
+          <input class="h-5 w-5 mr-4" name="shareTo" type="radio" v-model="shareTo" value="selected">
+          <span class="text-base">Select users</span>
+        </label>
+      </div>
+    </div>
+  </div>
+
+  <div class="w-modal-width-sm max-h-64 overflow-y-auto">
+    <!--Web followers-->
+    <div v-if="shareTo === 'selected' && shareType === 'web' && webFollowers.length"
+         class="mt-4 flex flex-row flex-wrap ml-10 mr-14">
+      <WebGridItem
+          v-for="follower in sortedWebFollowers"
+          :key="follower.getUniqueId()"
+          class="pl-4 pt-4 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6"
+          :webFollower="follower"
+          :controls="false"
+          @update="(value: boolean) => { handleFollowerSelection(follower.getUniqueId(), value) }"/>
+    </div>
+
+    <!--Mobile followers-->
+    <div v-else-if="shareTo === 'selected' && shareType === 'mobile' && mobileFollowers.length"
+         class="mt-4 flex flex-row flex-wrap ml-10 mr-14">
+      <MobileGridItem
+          v-for="follower in sortedMobileFollowers"
+          :key="follower.getUniqueId()"
+          class="pl-4 pt-4 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6"
+          :mobileFollower="follower"
+          :controls="false"
+          @update="(value: boolean) => { handleFollowerSelection(follower.getUniqueId(), value) }"/>
+    </div>
+
+    <!--No followers-->
+    <div class="flex justify-center items-center bg-gray-200 mx-14 mt-4 px-5 py-5" v-else-if="shareTo === 'selected'">
+      <span>No students connected</span>
+    </div>
+  </div>
+
+
+  <footer class="mt-11 mb-8 mx-14 text-right flex flex-row justify-between">
+    <!--Clear any tasks the mobile users may already have-->
+    <button class="w-36 h-11 mr-4 text-blue-500 text-base rounded-lg hover:bg-gray-default font-medium"
+            :class="{'invisible': shareType === 'web'}"
+            v-on:click="clearTaskList"
+    >Clear tasks</button>
+
+    <div class="flex flex-row">
+      <button class="w-36 h-11 mr-4 text-blue-500 text-base rounded-lg hover:bg-gray-default font-medium"
+              v-on:click="$emit('back');"
+      >Back</button>
+      <div class="relative">
+        <div
+            v-if="error !== undefined"
+            class="absolute bottom-12 min-w-max right-0 rounded shadow-lg
+            px-3 py-1 rounded-xl bg-white text-black border-gray-600
+            border-1 flex flex-row items-center mb-2">
+          <img
+              class="w-5 h-5 mr-2"
+              src="/src/assets/img/student-icon-alert.svg"
+              alt="alert icon"
+          />
+          <span>{{error}}</span>
         </div>
-      </template>
 
-      <template v-slot:footer>
-        <footer class="mt-11 mb-8 mx-14 text-right flex flex-row justify-between">
+        <GenericButton
+            class="w-52 h-12 text-white bg-blue-500 rounded-lg text-base hover:bg-blue-400 font-medium"
+            :class="{'mr-4': shareType === 'mobile' && viewDescription === ''}"
+            :callback="validateAndSubmit"
+        >{{ selectedItems.length > 1 && viewDescription === '' ? 'Add to tasks' : 'Share link' }}</GenericButton>
 
-          <!--Clear any tasks the mobile users may already have-->
-          <button class="w-36 h-11 mr-4 text-blue-500 text-base rounded-lg hover:bg-gray-default font-medium"
-                  :class="{'invisible': shareType === 'web'}"
-                  v-on:click="clearTaskList"
-          >Clear tasks</button>
-
-          <div class="flex flex-row">
-            <button class="w-36 h-11 mr-4 text-blue-500 text-base rounded-lg hover:bg-gray-default font-medium"
-                    v-on:click="showModal = false"
-            >Cancel</button>
-            <div class="relative">
-              <div
-                  v-if="error !== undefined"
-                  class="absolute bottom-12 min-w-max right-0 rounded shadow-lg
-                  px-3 py-1 rounded-xl bg-white text-black border-gray-600
-                  border-1 flex flex-row items-center mb-2">
-                <img
-                    class="w-5 h-5 mr-2"
-                    src="/src/assets/img/student-icon-alert.svg"
-                    alt="alert icon"
-                />
-                <span>{{error}}</span>
-              </div>
-
-              <GenericButton
-                  class="w-52 h-12 text-white bg-blue-500 rounded-lg text-base hover:bg-blue-400 font-medium"
-                  :class="{'mr-4': shareType === 'mobile' && viewDescription === ''}"
-                  :callback="validateAndSubmit"
-              >{{ selectedItems.length > 1 && viewDescription === '' ? 'Add to tasks' : 'Share link' }}</GenericButton>
-
-              <GenericButton
-                  v-if="shareType === 'mobile' && viewDescription === ''"
-                  class="w-52 h-12 text-white bg-blue-500 rounded-lg text-base hover:bg-blue-400 font-medium"
-                  :callback="shareCurrentList"
-              >Share list</GenericButton>
-            </div>
-          </div>
-        </footer>
-      </template>
-    </Modal>
-  </Teleport>
+        <GenericButton
+            v-if="shareType === 'mobile' && viewDescription === ''"
+            class="w-52 h-12 text-white bg-blue-500 rounded-lg text-base hover:bg-blue-400 font-medium"
+            :callback="shareCurrentList"
+        >Share list</GenericButton>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style>
--- src/components/Modals/ShareContent/ShareContentModal.vue
+++ src/components/Modals/ShareContent/ShareContentModal.vue
@@ -4,10 +4,21 @@ import Modal from "../Modal.vue";
 import { useDashboardStore } from "@/stores/dashboardStore";
 import {storeToRefs} from "pinia";
 import shareContentIconUrl from '/src/assets/img/sideMenu/menu-icon-sharecontent.svg';
+import ShareVideoInsert from "@/components/Modals/ShareContent/ShareVideoInsert.vue";
+import ShareApplicationInsert from "@/components/Modals/ShareContent/ShareApplicationInsert.vue";
+import ShareWebsiteInsert from "@/components/Modals/ShareContent/ShareWebsiteInsert.vue";
+import CuratedContentInsert from "@/components/Modals/ShareContent/CuratedContentInsert.vue";
+import curatedImageUrl from '/src/assets/img/share-content-curated.svg';
+import mobileIconUrl from '/src/assets/img/share-content-mobile.svg';
+import devicesIconUrl from '/src/assets/img/share-content-devices.svg';
+import linkIconUrl from '/src/assets/img/share-content-link.svg';
 
 const dashboardPinia = useDashboardStore();
+
+// TODO change the layout depending on the follower types/numbers
 const { webFollowers, mobileFollowers } = storeToRefs(dashboardPinia)
 const showModal = ref(false);
+const sharePanel = ref('menu');
 
 const classCode = computed(() => {
   return dashboardPinia.classCode !== ''
@@ -21,6 +32,7 @@ function openModal() {
 
 function closeModal() {
   showModal.value = false
+  sharePanel.value = 'menu';
 }
 </script>
 
@@ -66,31 +78,79 @@ function closeModal() {
       </template>
 
       <template v-slot:content>
-        <div class="w-modal-width-sm bg-gray-300 pb-3 px-3">
+        <div v-if="sharePanel === 'menu'" class="w-modal-width-sm bg-gray-300 pb-3 px-3">
           <!--Show the url input screen-->
-          <div class="flex justify-center items-center bg-white rounded-2xl p-1 h-16">
-            Share a Custom URL
+          <div class="flex justify-between items-center
+              text-gray-400 text-lg
+              bg-white rounded-2xl p-1 h-16 cursor-pointer">
+
+            <!--Placeholder to keep the url text centered-->
+            <div class="w-5 h-5 ml-5"></div>
+
+            <div
+              class="flex justify-center items-center"
+              v-on:click="sharePanel = 'url'"
+            >
+              <img class="w-5 h-5 mr-3" :src="linkIconUrl" alt="Icon"/>
+              Share a Custom URL
+            </div>
+
+            <img class="w-5 h-5 mr-5" :src="devicesIconUrl" alt="Icon"/>
           </div>
 
           <div class="flex mt-2 h-96">
             <!--Show the curated content screen-->
-            <div class="w-1/2 p-1 mr-1 flex justify-center items-center bg-white text-center rounded-2xl">
-              Curated Content
+            <div
+              class="w-1/2 p-1 mr-1 flex flex-col
+              justify-center items-center
+              bg-white text-center rounded-2xl
+              cursor-pointer"
+              v-on:click="sharePanel = 'curated'"
+            >
+              <img class="w-48 h-48 mb-3" :src="curatedImageUrl" alt="Icon"/>
+              <div class="text-lg flex flex-row items-center mb-3">
+                Curated Content
+                <img class="w-5 h-5 ml-2" :src="devicesIconUrl" alt="Icon"/>
+              </div>
+              <div class="text-sm text-gray-400">Curated immersive learning videos.</div>
             </div>
 
             <div class="w-1/2 h-full ml-1 flex flex-col">
               <!--Show the local video screen-->
-              <div class="h-1/2 flex justify-center items-center bg-white text-center rounded-2xl">
-                Videos
+              <div
+                class="h-1/2 flex flex-col justify-center
+                items-center bg-white text-center
+                rounded-2xl cursor-pointer"
+                v-on:click="sharePanel = 'video'"
+              >
+                <div class="text-lg flex flex-row items-center mb-3">
+                  Videos
+                  <img class="w-5 h-5 ml-2" :src="mobileIconUrl" alt="Icon"/>
+                </div>
+                <div class="text-sm text-gray-400">Local videos on mobile.</div>
               </div>
 
               <!--Show the local application screen-->
-              <div class="h-1/2 flex justify-center items-center mt-2 bg-white text-center rounded-2xl">
-                Apps
+              <div
+                class="h-1/2 flex flex-col justify-center
+                items-center mt-2 bg-white text-center
+                rounded-2xl cursor-pointer"
+                v-on:click="sharePanel = 'app'"
+              >
+                <div class="text-lg flex flex-row items-center mb-3">
+                  Apps
+                  <img class="w-5 h-5 ml-2" :src="mobileIconUrl" alt="Icon"/>
+                </div>
+                <div class="text-sm text-gray-400">Immersive learning apps.</div>
               </div>
             </div>
           </div>
         </div>
+
+        <ShareWebsiteInsert v-else-if="sharePanel === 'url'" @back="sharePanel = 'menu'"/>
+        <CuratedContentInsert v-else-if="sharePanel === 'curated'" @back="sharePanel = 'menu'"/>
+        <ShareVideoInsert v-else-if="sharePanel === 'video'" @back="sharePanel = 'menu'"/>
+        <ShareApplicationInsert v-else-if="sharePanel === 'app'" @back="sharePanel = 'menu'"/>
       </template>
     </Modal>
   </Teleport>
