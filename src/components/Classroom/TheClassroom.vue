a9ff7d21d320fd95a709aa5e33a683af1888c2ce
Edward Coad <ecoad@lumination.com.au>
2023-07-07 08:11:12 +0930

    Fixed side menu bug

--- src/components/Classroom/TheClassroom.vue
+++ src/components/Classroom/TheClassroom.vue
@@ -1,27 +1,25 @@
 <script setup lang="ts">
 import ClassroomSideMenu from "./SideMenu/ClassroomSideMenu.vue";
-import { onBeforeMount, onMounted, ref } from "vue";
+import {computed, onBeforeMount, onMounted, ref} from "vue";
 import { getAuth } from "@firebase/auth";
 import { useClassroomStore } from "@/stores/classroomStore";
 import { useRouter } from "vue-router";
 
 const classroomPinia = useClassroomStore();
 const router = useRouter()
-const emailVerified = ref(false)
 const loaded = ref(false)
 
-// todo this runs on the login page, so does not re-run when logging in (first time load the sidemenu does not appear)
+const emailVerified = computed(() => {
+  return classroomPinia.user?.emailVerified ?? false
+});
+
 //Check for any active class on load
 onMounted(async () => {
   classroomPinia.loadCuratedContent()
   await classroomPinia.onLoad()
   await classroomPinia.attachClassListeners(true)
   const auth = getAuth()
-
   classroomPinia.user = auth.currentUser
-  if (classroomPinia.user) {
-    emailVerified.value = classroomPinia.user.emailVerified
-  }
   loaded.value = true
 });
 
